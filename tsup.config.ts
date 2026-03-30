9ef5929db7dc5fd0fd081fc9d3b56efd54e340e1
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2026-03-30 20:58:43 +0500

    fix: publish cli-only package entrypoint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/cli/index.ts'],
   format: ['esm'],
   splitting: false,
   clean: true,
